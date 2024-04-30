38f275e4b53d87b478fe1fa1262f4533511dff28
Harnassus <micfestus45@gmail.com>
2024-04-30 01:18:15 +0100

    Setup CORS

--- main.py
+++ main.py
@@ -2,12 +2,32 @@ import os
 from fastapi import FastAPI
 from fastapi_extras.errors import configure_error_handlers
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 
 if "GOOGLE_API_KEY" not in os.environ:
     raise Exception("GOOGLE_API_KEY not provided in the env")
+
+
+def configure_cors(app: FastAPI):
+    origin_env = os.environ.get("CORS_ORIGINS")
+
+    origins = []
+
+    if not origin_env:
+        origins = ["http://localhost:5173"]
+
+    origins = origin_env.split(",")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 def configure_router(app: FastAPI):
@@ -19,6 +39,7 @@ def configure_router(app: FastAPI):
 def create_app() -> FastAPI:
     app = FastAPI()
     configure_router(app)
+    configure_cors(app)
     configure_error_handlers(app)
 
     return app
@@ -30,7 +51,4 @@ app = create_app()
 if __name__ == "__main__":
     import uvicorn
 
-    debug = os.environ.get("DEBUG") or False
-    port = os.environ.get("PORT") or 8080
-
-    uvicorn.run("main:app", port=port, reload=debug)
+    uvicorn.run("main:app", port=8000, reload=True)
